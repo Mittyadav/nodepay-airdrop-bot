fff51c6f1e0756daac45daa679816f9d7488cd45
Mittyadav <146659530+Mittyadav@users.noreply.github.com>
2024-11-11 16:23:38 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -16,9 +16,9 @@ async function readLines(filename) {
 function displayHeader() {
   process.stdout.write('\x1Bc');
   console.log('========================================'.cyan);
-  console.log('=          Nodepay Airdrop Bot         ='.cyan);
-  console.log('=     Created by HappyCuanAirdrop      ='.cyan);
-  console.log('=    https://t.me/HappyCuanAirdrop     ='.cyan);
+  console.log('=        Nodepay Airdrop Bot           ='.cyan);
+  console.log('=     Created by dark life             ='.cyan);
+  console.log('=    https://t.me/scripthub00          ='.cyan);
   console.log('========================================'.cyan);
   console.log();
 }
