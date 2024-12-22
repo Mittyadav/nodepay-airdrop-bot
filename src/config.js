70d6e05b67270d61c494ac5ca2d2ed07059fe9e5
@mitt_ydv <146659530+Mittyadav@users.noreply.github.com>
2024-12-22 18:45:54 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,13 +2,7 @@ class Config {
   constructor() {
     this.baseURL = 'https://nodepay.org';
     this.ipCheckURL = 'https://ipinfo.io/json';
-    this.pingURL = 'http://52.77.10.116/api/network/ping',
-        'http://13.215.134.222/api/network/ping',
-        'http://18.136.143.169/api/network/ping',
-        'http://52.74.35.173/api/network/ping',
-        'http://18.142.214.13/api/network/ping',
-        'http://18.142.29.174/api/network/ping',
-        'http://52.74.31.107/api/network/ping';
+    this.pingURL = 'http://52.74.31.107/api/network/ping';        
     this.retryInterval = 30000;
     this.sessionURL = 'http://api.nodepay.ai/api/auth/session';
   }
